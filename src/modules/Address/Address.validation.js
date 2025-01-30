9ad2fa2583b18d4c47c5dd9ca0c05a0c0c09bda2
abood1aa <aa2949139@gmail.com>
2025-01-31 01:39:22 +0200

    Update Address.validation.js

--- src/modules/Address/Address.validation.js
+++ src/modules/Address/Address.validation.js
@@ -1,1 +1,26 @@
+const Joi = require('joi');
+
+const addressSchema = Joi.object({
+  city: Joi.string().required().messages({
+    'string.base': 'City must be a string',
+    'any.required': 'City is required',
+  }),
+  street: Joi.string().required().messages({
+    'string.base': 'Street must be a string',
+    'any.required': 'Street is required',
+  }),
+  phone: Joi.string().pattern(/^\d+$/).required().messages({
+    'string.pattern.base': 'Phone must contain only numbers',
+    'any.required': 'Phone is required',
+  }),
+});
+
+const userSchema = Joi.object({
+  addresses: Joi.array().items(addressSchema).required().messages({
+    'array.base': 'Addresses must be an array',
+    'any.required': 'Addresses are required',
+  }),
+});
+
+module.exports = userSchema;
 
