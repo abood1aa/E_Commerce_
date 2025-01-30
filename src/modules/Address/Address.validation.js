3105c35945120cafd7cca01c7dd7f707112f5387
abood1aa <aa2949139@gmail.com>
2025-01-31 01:33:19 +0200

    Update Address.validation.js

--- src/modules/Address/Address.validation.js
+++ src/modules/Address/Address.validation.js
@@ -1,10 +1,1 @@
-import joi from "joi";
-import { generalFiled } from "../../../Utility/genarica.js";
 
-
-export const createWishList = {
-    params: joi.object({
-        productId: generalFiled.id.required(),
-    }).required(),
-    headers: generalFiled.headers.required()
-}
